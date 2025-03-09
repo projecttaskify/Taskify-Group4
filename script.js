17c1201aeee94a5426e409531c8ed19b847aa9b3
Brian Al Tenorio <152787704+projecttaskify@users.noreply.github.com>
2025-03-09 10:09:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ document.addEventListener("DOMContentLoaded", () => {
         renderTasks();
     }
 
-    /**
+    /*
       - Function to delete a task
       - Asks for confirmation before deleting
      */
