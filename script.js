5a86b3e64bf0a97e39fe9948f0e909ee12e648a8
LonMonserate <lamonserate703.pbox@parsu.edu.ph>
2025-03-16 12:32:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
 // Select Elements
     const taskInput = document.getElementById("task-input");
     const addTaskButton = document.getElementById("add-task");
